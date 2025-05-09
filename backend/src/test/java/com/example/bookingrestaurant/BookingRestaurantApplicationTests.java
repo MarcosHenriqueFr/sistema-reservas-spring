0995df08649548eaa15bd98e8402030349feaaf2
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-09 15:27:06 -0300

    initial configs

---
+++ backend/src/test/java/com/example/bookingrestaurant/BookingRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
