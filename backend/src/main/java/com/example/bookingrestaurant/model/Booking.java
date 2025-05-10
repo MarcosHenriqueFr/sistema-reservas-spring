4d953fb3dae133abaaa696c9d0a85aa3e47f9143
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-10 11:48:21 -0300

    feat: add jpa Entity to Booking Class

--- backend/src/main/java/com/example/bookingrestaurant/model/Booking.java
+++ backend/src/main/java/com/example/bookingrestaurant/model/Booking.java
@@ -1,41 +1,72 @@
 package com.example.bookingrestaurant.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "bookings")
 public class Booking {
 
-    private int id;
-    private int user_id;
-    private int table_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Ambas serem relações 1 - 1
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true)
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_table_id", referencedColumnName = "id", unique = true)
+    private RestaurantTable table;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime booking_date;
+
+    @Enumerated(EnumType.STRING)
     private BookingStatus bookingStatus;
 
     public Booking(){
 
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public int getUser_id() {
-        return user_id;
+    public User getUser() {
+        return user;
     }
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public int getTable_id() {
-        return table_id;
+    public RestaurantTable getTable() {
+        return table;
     }
 
-    public void setTable_id(int table_id) {
-        this.table_id = table_id;
+    public void setTable(RestaurantTable table) {
+        this.table = table;
     }
 
     public LocalDateTime getBooking_date() {
