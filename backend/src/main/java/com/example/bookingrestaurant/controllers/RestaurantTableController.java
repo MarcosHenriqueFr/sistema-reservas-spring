5753e9068ae7fb61f8082b69c5cbd7613261e64c
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-13 19:08:32 -0300

    feat: adds Delete and Patch Methods according to the service layer

--- backend/src/main/java/com/example/bookingrestaurant/controllers/RestaurantTableController.java
+++ backend/src/main/java/com/example/bookingrestaurant/controllers/RestaurantTableController.java
@@ -2,11 +2,14 @@ package com.example.bookingrestaurant.controllers;
 
 import com.example.bookingrestaurant.dto.RestaurantTableDTO;
 import com.example.bookingrestaurant.model.RestaurantTable;
-import com.example.bookingrestaurant.repositories.RestaurantTableRepository;
+import com.example.bookingrestaurant.services.RestaurantTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +22,33 @@ import java.util.List;
 public class RestaurantTableController {
 
     @Autowired
-    private RestaurantTableRepository restaurantTableRepository;
+    private RestaurantTableService tableService;
 
     @GetMapping
     public ResponseEntity<List<RestaurantTable>> getAllTables(){
-        List<RestaurantTable> tables = restaurantTableRepository.findAll();
+        List<RestaurantTable> tables = tableService.getAllTables();
         return new ResponseEntity<>(tables, HttpStatus.OK);
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<RestaurantTable> createTable(@RequestBody RestaurantTableDTO data){
-        RestaurantTable newTable = new RestaurantTable(data);
-        restaurantTableRepository.save(newTable);
+        RestaurantTable newTable = tableService.createRestaurantTable(data);
         return new ResponseEntity<>(newTable, HttpStatus.CREATED);
+    }
+
+    // TODO Fazer o tratamento de Exceções/Criacao -> No delete e no patch
+
+    @PatchMapping(path = ":{id}")
+    public ResponseEntity<RestaurantTable> updateTable(@PathVariable Long id, @RequestBody RestaurantTableDTO updates) throws Exception {
+        RestaurantTable modifiedTable = tableService.updateTable(id, updates);
+        return new ResponseEntity<>(modifiedTable, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(path = ":{id}")
+    public ResponseEntity<RestaurantTable> deleteTable(@PathVariable Long id) throws Exception {
+        RestaurantTable table = tableService.findTableById(id);
+        tableService.deleteTable(table);
+        return new ResponseEntity<>(table, HttpStatus.NO_CONTENT);
     }
 }
