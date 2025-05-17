0607cff9400205e1ed015a1d41f5111dbaf6587c
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-17 20:10:37 -0300

    feat: add standard endpoint security configuration

---
+++ backend/src/main/java/com/example/bookingrestaurant/config/security/config/SecurityConfig.java
@@ -0,0 +1,95 @@
+package com.example.bookingrestaurant.config.security.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String ADMIN_ROLE = "ADMINISTRATOR";
+    private final String CUSTOMER_ROLE = "CUSTOMER";
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey key;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey priv;
+
+    private final String[] ENDPOINT_WITHOUT_AUTH = {
+            "/usuarios/login",
+            "/usuarios/registrar"
+    };
+
+    private final String[] ENDPOINT_WITH_AUTH = {
+            "/mesas"
+    };
+
+    // O mét0do que controla toda a segurança
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers(HttpMethod.POST, ENDPOINT_WITHOUT_AUTH).permitAll()
+                                        .requestMatchers(HttpMethod.POST, ENDPOINT_WITH_AUTH).hasRole(ADMIN_ROLE)
+                                        .requestMatchers(HttpMethod.GET, ENDPOINT_WITH_AUTH).hasAnyRole(ADMIN_ROLE, CUSTOMER_ROLE)
+                                        .anyRequest().denyAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(
+                        conf -> conf.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter grantedAuthorities = new JwtGrantedAuthoritiesConverter();
+        grantedAuthorities.setAuthoritiesClaimName("scope");
+        grantedAuthorities.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthorities);
+        return authenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(key).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        RSAKey jwk = new RSAKey.Builder(key).privateKey(priv).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
