e072bcc9bc4f9e0424234b76905abf16fa1d48af
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-18 20:05:56 -0300

    feat: add a new Exception for getting user by email

--- backend/src/main/java/com/example/bookingrestaurant/config/security/userdetails/UserAuthenticated.java
+++ backend/src/main/java/com/example/bookingrestaurant/config/security/userdetails/UserAuthenticated.java
@@ -51,4 +51,8 @@ public class UserAuthenticated implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public User getUser() {
+        return user;
+    }
 }
--- backend/src/main/java/com/example/bookingrestaurant/services/UserService.java
+++ backend/src/main/java/com/example/bookingrestaurant/services/UserService.java
@@ -1,11 +1,14 @@
 package com.example.bookingrestaurant.services;
 
 import com.example.bookingrestaurant.config.security.config.SecurityConfig;
+import com.example.bookingrestaurant.config.security.userdetails.UserAuthenticated;
+import com.example.bookingrestaurant.config.security.userdetails.UserDetailsServiceImpl;
 import com.example.bookingrestaurant.dto.UserPostDTO;
 import com.example.bookingrestaurant.model.RoleName;
 import com.example.bookingrestaurant.model.User;
 import com.example.bookingrestaurant.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Pattern;
@@ -19,9 +22,20 @@ public class UserService {
     @Autowired
     private SecurityConfig securityConfig;
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
     // TODO melhorar as Exceptions
 
-    public void createUser(UserPostDTO dto) throws Exception {
+    public UserAuthenticated getUserByEmail(String email) throws Exception{
+        try {
+            return (UserAuthenticated) userDetailsService.loadUserByUsername(email);
+        } catch (UsernameNotFoundException e){
+            throw new Exception("Usuário não encontrado!");
+        }
+    }
+
+    public User createUser(UserPostDTO dto) throws Exception {
         String name = dto.name();
         String email = dto.email();
         String password = securityConfig.passwordEncoder().encode(dto.password());
@@ -34,9 +48,11 @@ public class UserService {
         user.setRole(role);
 
         this.saveUser(user);
+
+        return user;
     }
 
-    public void saveUser(User user){
+    private void saveUser(User user){
         userRepository.save(user);
     }
 }
