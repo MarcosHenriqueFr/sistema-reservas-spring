50ce0b38d29e1cd5aa21cca7ee6f6122ff80c515
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-05-25 12:42:24 -0300

    feat: add validation for RestaurantTableDTO

--- backend/src/main/java/com/example/bookingrestaurant/dto/RestaurantTableDTO.java
+++ backend/src/main/java/com/example/bookingrestaurant/dto/RestaurantTableDTO.java
@@ -1,7 +1,17 @@
 package com.example.bookingrestaurant.dto;
 
 import com.example.bookingrestaurant.model.RestaurantTableStatus;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Range;
 
-public record RestaurantTableDTO(String name, int capacity, RestaurantTableStatus status) {
+public record RestaurantTableDTO(
+        @Size(min = 3, message = "O código da mesa precisa de pelo menos três caracteres")
+        @NotEmpty(message = "Informe o nome/código da mesa")
+        String name,
 
-}
+        @Range(min = 2, max = 20, message = "A capacidade da mesa tem que ser entre 2 e 20")
+        int capacity,
+
+        RestaurantTableStatus status
+) { }
